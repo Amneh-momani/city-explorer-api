1a22749659e7d52dbcf06b57ae8fbcb5031a6339
Amneh-momani <Amneh99el@gmail.com>
2021-08-04 20:42:39 -0700

    lab8

---
+++ models/Moive.js
@@ -0,0 +1,13 @@
+class Moive {
+  constructor(value) {
+    this.title=value.title;
+    this.overview=value.overview;
+        this.average_votes=value.average_votes;
+    this.total_votes=value.total_votes;
+    this.image_url=value.image_url;
+    this.popularity=value.popularity;
+    this.released_on= value.released_on;
+  }
+}
+
+module.exports = Moive;
