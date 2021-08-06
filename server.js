73da4dea24b5372c97dda3c2ede9d60b36670e03
Amneh-momani <Amneh99el@gmail.com>
2021-08-05 17:41:16 -0700

    update1

--- server.js
+++ server.js
@@ -1,43 +1,21 @@
 "use strict";
-
-const { request, response } = require("express");
 const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
 const server = express();
+require("dotenv").config();
 const axios = require("axios");
-
+const cors = require("cors");
 server.use(cors());
 
-const data = require("./data/weather.json");
 const Weather = require("./models/Weather");
 const Moive = require("./models/Moive");
-const Forecast = require("./models/Forecast");
 const PORT = process.env.PORT;
 const WEATHER_URL = process.env.WEATHER_URL;
 const WEATHER_KEY = process.env.WEATHER_KEY;
-
 const MOIVE_KEY = process.env.MOIVE_KEY;
 const MOIVE_URL = process.env.MOIVE_URL;
 
 server.get("/", (req, res) => {
   res.send("Hello from main page in backend");
-});
-
-server.get("/weather", (req, res) => {
-  try {
-    let { searchQuery, lat, lon } = req.query;
-    let cityData = data.find(
-      (element) =>
-        element.city_name.toLowerCase() === searchQuery.toLowerCase() ||
-        (`${element.lat}` === lat && `${element.lon}` === lon)
-    );
-    let forecastArr = cityData.data.map((items) => new Forecast(items));
-    console.log(forecastArr);
-    res.send(forecastArr);
-  } catch (e) {
-    res.status(404).send("No Data for this City");
-  }
 });
 
 server.get("/weather-bit", async (req, res) => {
@@ -49,24 +27,22 @@ server.get("/weather-bit", async (req, res) => {
       lon: lon,
     },
   };
-  const response = await axios.get(`${(WEATHER_URL, para)}`);
-  const data = response.data.data.map((items) => new Weather(items));
-  console.log(data);
-  res.json(data);
+  const response = await axios.get(WEATHER_URL,para);
+  const dataWeather = response.data.data.map((items) => new Weather(items));
+  res.json(dataWeather);
 });
 
-server.get("/moive", async (req, res) => {
-  const { MOIVE_KEY, searchQ } = req.query;
+server.get("/movie", async (req, res) => {
+  const { MOIVE_KEY, city_name } = req.query;
   const para = {
     parameter: {
       api_key: MOIVE_KEY,
       query: city_name,
     },
   };
-  const response = await axios.get(`${(MOIVE_URL, para)}`);
-  const data = response.data.data.map((items) => new Moive(items));
-  console.log(response);
-  res.json(data);
+  const response = await axios.get(MOIVE_URL,para);
+  const dataMoive = response.data.data.map((items) => new Moive(items));
+  res.json(dataMoive);
 });
 
 server.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
