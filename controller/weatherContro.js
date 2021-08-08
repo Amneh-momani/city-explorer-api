77065b5a6cd91acb8a6aa6da7b0ec49a93ae64b7
Amneh-momani <Amneh99el@gmail.com>
2021-08-08 01:38:58 -0700

    cache

--- controller/weatherContro.js
+++ controller/weatherContro.js
@@ -3,6 +3,9 @@ const WEATHER_KEY = process.env.WEATHER_KEY;
 
 const axios = require("axios");
 const Weather = require("../models/Weather");
+
+const Cache = require("../helpers/cache");
+let cacheObject = new Cache();
 WeatherContro = (req, res) => {
   const { lat, lon } = req.query;
   const para = {
@@ -14,7 +17,35 @@ WeatherContro = (req, res) => {
   };
   const response = axios.get(WEATHER_URL, para);
   const dataWeather = response.data.data.map((items) => new Weather(items));
-  res.json(dataWeather);
+  cacheObject.weather.push({
+    lat: lat,
+    lon: lon,
+    data: data,
+  });
+
+  return data;
+};
+const getWeather = async (req, res) => {
+  const { lat, lon } = req.query;
+
+  if (Date.now() - cacheObject.timeStamp > 86400000) {
+    console.log("Reset Cache");
+    cacheObject = new Cache();
+  }
+  if (cacheObject.weather.length) {
+    const filteredData = cacheObject.weather.find((location) => {
+      return location.lat === lat && location.lon === lon;
+    });
+
+    if (filteredData) {
+      console.log("getting the data from the cache");
+      res.json(filteredData.data);
+    } else {
+      res.json(await getWeatherBitData(lat, lon));
+    }
+  } else {
+    res.json(await getWeatherBitData(lat, lon));
+  }
 };
 
 module.exports = WeatherContro;
